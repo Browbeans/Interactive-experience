21b146ebcadf9fbdfbeff7b99ea8cb75a0bcf33f
Browbeans <oliverjohansson3@gmail.com>
2020-10-26 09:39:46 +0100

    Created html-file, css-file and js-file

---
+++ script.js
@@ -0,0 +1,9 @@
+function getValueMale() {
+    const div = document.getElementById ("generateContent")
+    div.append("You have now selected male" + ".")
+}
+
+function getValueFemale() {
+    const div = document.getElementById ("generateContent")
+    div.append("You have now selected Female" + "." )
+}
