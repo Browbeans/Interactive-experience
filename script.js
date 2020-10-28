eb967e01b961e1e68770c4fe35afac5998fb3a7c
Browbeans <oliverjohansson3@gmail.com>
2020-10-28 10:37:53 +0100

    add form function

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 function getValueStay() {
     let stay = document.getElementById("stayHere");
     if(stay.style.display == 'block')
-       stay.style.display = 'none';
+       stay.style.display = 'none'
     else
-       stay.style.display = 'block';
+       stay.style.display = 'block'
  }
 
 let valuesBrochureSign = ["BROCHURE", "SIGN"]
@@ -14,25 +14,33 @@ function choseBrochureSign() {
     if (choice.toUpperCase() == valuesBrochureSign[0]){
         let brochure = document.getElementById ("actionBrochure")
         if(brochure.style.display == 'block')
-           brochure.style.display = 'none';
+            brochure.style.display = 'none'
         else
-           brochure.style.display = 'block';
+            brochure.style.display = 'block'
     }
     else if (choice.toUpperCase() == valuesBrochureSign[1]){
-        let sign = document.getElementById("actionSign");
+        let sign = document.getElementById("actionSign")
         if(sign.style.display == 'block')
-        sign.style.display = 'none';
+        sign.style.display = 'none'
     else
-       sign.style.display = 'block';
+       sign.style.display = 'block'
     }
+}
+
+function validateForm(){
+    let stay = document.getElementById("formValidated");
+    if(stay.style.display == 'block')
+       stay.style.display = 'none'
+    else
+       stay.style.display = 'block'
 }
 
 function getValueRun() {
     var run = document.getElementById("runAway");
     if(run.style.display == 'block')
-       run.style.display = 'none';
+       run.style.display = 'none'
     else
-       run.style.display = 'block';
+       run.style.display = 'block'
  }
 
 let valuesPTI = ["PUNCH", "TALK", "IGNORE"]
@@ -42,29 +50,39 @@ function chosePunchTalkIgnore() {
     if (input.toUpperCase() == valuesPTI[0]){     
         let punch = document.getElementById("actionPunch")
         if(punch.style.display == 'block')
-           punch.style.display = 'none';
+           punch.style.display = 'none'
         else
-           punch.style.display = 'block';
+           punch.style.display = 'block'
     }
 
     else if (input.toUpperCase() == valuesPTI[1]){     
         let run = document.getElementById("stayHere");
         if(run.style.display == 'block')
-        run.style.display = 'none';
+        run.style.display = 'none'
     else
-       run.style.display = 'block';
+       run.style.display = 'block'
     }
 
     else if (input.toUpperCase() == valuesPTI[2]){     
         let chooseFightOrDie = document.getElementById("actionIgnore");
         if(chooseFightOrDie.style.display == 'block')
-            chooseFightOrDie.style.display = 'none';
+            chooseFightOrDie.style.display = 'none'
         else
-            chooseFightOrDie.style.display = 'block';
+            chooseFightOrDie.style.display = 'block'
     }
     else {
         let wrongValue = document.getElementById("runAway")
         wrongValue.append("Please enter Punch, Talk or Ignore")
-
     }
 }
+
+function restartYes() {
+    location.reload();
+}
+
+
+// let noRestart = document.getElementById("resartNO")
+// noRestart.onclick() =  function endPage() {
+//     document.querySelector 
+// }
+
