f5b7f05b132c7a6b09dc814e2dc01ce54d7306ae
Browbeans <oliverjohansson3@gmail.com>
2020-11-13 12:37:49 +0100

    Fixed bug

---
+++ script/script.js
@@ -0,0 +1,136 @@
+/**
+ * Will display "stay here div" based on users input
+ */
+function getValueStay() {
+    let stay = document.getElementById("stayHere");
+    if(stay.style.display == 'block')
+       stay.style.display = 'none'
+    else
+       stay.style.display = 'block'
+ }
+
+/**
+ * Array of choices that the users has
+ */ 
+let valuesBrochureSign = ["BROCHURE", "SIGN"]
+
+/**
+ * Will display one div based on users choice either "actionBrochure", "actionSign" or display a wrong message if user types in invalid value.  
+ */
+function choseBrochureSign() {
+    let choice = document.getElementById("getBrochureOrSign").value;
+    if (choice.toUpperCase() == valuesBrochureSign[0]){
+        let brochure = document.getElementById ("actionBrochure")
+        if(brochure.style.display == 'block')
+            brochure.style.display = 'none'
+        else
+            brochure.style.display = 'block'
+    }
+    else if (choice.toUpperCase() == valuesBrochureSign[1]){
+        let sign = document.getElementById("actionSign")
+        if(sign.style.display == 'block')
+        sign.style.display = 'none'
+    else
+       sign.style.display = 'block'
+    }
+    else {
+        let wrongValue = document.getElementById("stayHere")
+        wrongValue.append("Please enter brochure or sign")
+    }
+}
+
+/**
+ * Will display the div called "formValidated" if the user chooses to sign the form
+ */
+function validateForm(){
+    let stay = document.getElementById("formValidated");
+    if(stay.style.display == 'block')
+       stay.style.display = 'none'
+    else
+       stay.style.display = 'block'
+}
+
+/**
+ * Will display the div called "runAway" where the users have 3 choices
+ */
+function getValueRun() {
+    var run = document.getElementById("runAway");
+    if(run.style.display == 'block')
+       run.style.display = 'none'
+    else
+       run.style.display = 'block'
+ }
+
+
+/**
+ * array of choices "PUNCH" "TALK" and "IGNORE" 
+ */  
+let valuesPTI = ["PUNCH", "TALK", "IGNORE"]
+
+/**
+ * Will display one of 3 divs "actionPunch", "stayHere", "actionIgnore" or display a wrong message if user types in invalid value based on the users selection
+ */
+function chosePunchTalkIgnore() {
+    let input = document.getElementById("PTI").value;
+    if (input.toUpperCase() == valuesPTI[0]){     
+        let punch = document.getElementById("actionPunch")
+        if(punch.style.display == 'block')
+           punch.style.display = 'none'
+        else
+           punch.style.display = 'block'
+    }
+
+    else if (input.toUpperCase() == valuesPTI[1]){     
+        let run = document.getElementById("stayHere");
+        if(run.style.display == 'block')
+        run.style.display = 'none'
+    else
+       run.style.display = 'block'
+    }
+
+    else if (input.toUpperCase() == valuesPTI[2]){     
+        let chooseFightOrDie = document.getElementById("actionIgnore");
+        if(chooseFightOrDie.style.display == 'block')
+            chooseFightOrDie.style.display = 'none'
+        else
+            chooseFightOrDie.style.display = 'block'
+    }
+    else {
+        let wrongValue = document.getElementById("runAway")
+        wrongValue.append("Please enter Punch, Talk or Ignore")
+    }
+}
+
+/**
+ * Displays a div based on if user chooses pick up the stick
+ */
+function pickUpStick() {
+    let pickStick = document.getElementById("fightWithStick")
+    if(pickStick.style.display == 'block')
+        pickStick.style.display = 'none'
+    else
+        pickStick.style.display = 'block'
+}
+
+
+/**
+ * Displays a div based on if user chooses not to pick up the stick
+ */
+function ignoreStick() {
+    let ignorestick = document.getElementById("noStick")
+    if(ignorestick.style.display == 'block')
+        ignorestick.style.display = 'none'
+    else
+        ignorestick.style.display = 'block'
+}
+
+let stageIndex = 0 
+
+
+/**
+ * Function that will reload the page and start the game over
+ */
+function restartYes() {
+    location.reload();
+}
+
